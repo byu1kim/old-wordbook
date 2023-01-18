79b1128b5556ab043d109b1c34f76551dd3a2934
Byul Kim <caxbelle@gmail.com>
2023-01-17 20:47:32 -0800

    react connect

--- server.js
+++ server.js
@@ -1,10 +1,44 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
+const wordbook = [
+  {
+    id: 1,
+    english: "key",
+    korean: "열쇠",
+  },
+  {
+    id: 2,
+    english: "instagram",
+    korean: "인스타그램",
+  },
+  {
+    id: 3,
+    english: "find",
+    korean: "찾다",
+  },
+  {
+    id: 1,
+    english: "key",
+    korean: "열쇠",
+  },
+];
+
+app.use(express.static("build")); // build: react build 후 생겨난 폴더 이름
+
+app.get("/api/words", (req, res) => {
   console.log("GET /");
-  res.send("<h1>This is Word Book</h1>");
+  res.send(wordbook);
 });
+
+app.get("/*", (req, res) => {
+  res.sendFile("build/index.html");
+});
+
+// app.post("/api/words", (req, res) => {
+
+//   res.send("<h1>This is Word Book</h1>");
+// });
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`✅ Listening on PORT ${PORT}`));
