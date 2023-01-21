493d57ea6a77e28105832c62a476db22f50fe290
Byul Kim <caxbelle@gmail.com>
2023-01-21 06:15:50 -0800

    port edited

--- database.js
+++ database.js
@@ -9,7 +9,6 @@ const pool = mysql
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT || 3306,
   })
   .promise();
 
