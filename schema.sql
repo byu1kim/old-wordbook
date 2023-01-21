ebbb9bd6338e1d45a7f2521809f290dc61e6b8bb
Byul Kim <caxbelle@gmail.com>
2023-01-21 15:13:18 -0800

    rebuild with minor error

--- schema.sql
+++ schema.sql
@@ -14,7 +14,26 @@ DROP TABLE IF EXISTS words;
 
 CREATE TABLE words (
   id integer PRIMARY KEY AUTO_INCREMENT,
-  eng VARCHAR(255) NOT NULL,
-  kor VARCHAR(255) NOT NULL,
+  eng VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  kor VARCHAR(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL ,
   created TIMESTAMP NOT NULL DEFAULT NOW()
 );
+
+PORT=8090
+MYSQL_HOST="127.0.0.1"
+MYSQL_USER="wordbook_user"
+MYSQL_PASSWORD="MyPassword1!"
+MYSQL_DATABASE="wordbook"
+
+-- Change a database
+ALTER DATABASE wordbook
+  CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci; 
+
+-- Change a table
+ALTER TABLE words 
+  CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci; 
+
+-- Change a column
+ALTER TABLE words
+  CHANGE kor VARCHAR(255) 
+  CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
